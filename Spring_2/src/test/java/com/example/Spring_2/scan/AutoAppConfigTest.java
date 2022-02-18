a67ef3e2bc34b91bef481221175af804a90e9d72
seungyeonpark <siaayeon@gmail.com>
2022-02-18 23:46:07 +0900

    test: 컴포넌트 스캔과 의존관계 자동 주입

---
+++ Spring_2/src/test/java/com/example/Spring_2/scan/AutoAppConfigTest.java
@@ -0,0 +1,18 @@
+package com.example.Spring_2.scan;
+
+import com.example.Spring_2.AutoAppConfig;
+import com.example.Spring_2.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
