9d110a67cae6575e4579251d67a8f0871ed410db
seungyeonpark <siaayeon@gmail.com>
2022-02-18 23:00:24 +0900

    test: @Configuration과 싱글톤

---
+++ Spring_2/src/test/java/com/example/Spring_2/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,37 @@
+package com.example.Spring_2.singleton;
+
+import com.example.Spring_2.AppConfig;
+import com.example.Spring_2.member.MemberRepository;
+import com.example.Spring_2.member.MemberServiceImpl;
+import com.example.Spring_2.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        // 모두 같은 인스턴스를 참조
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // AppConfig도 스프링 빈으로 등록된다
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+    }
+}
