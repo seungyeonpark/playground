ab37ac8143f7c11a80cace544cff5707c299eae3
seungyeonpark <siaayeon@gmail.com>
2022-02-18 22:59:54 +0900

    test: 싱글톤 컨테이너

---
+++ Spring_2/src/test/java/com/example/Spring_2/singleton/SingletonTest.java
@@ -0,0 +1,59 @@
+package com.example.Spring_2.singleton;
+
+import com.example.Spring_2.AppConfig;
+import com.example.Spring_2.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+
+        AppConfig appConfig = new AppConfig();
+
+        // 1. 조회: 호출할 때 마다 객체 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        // 2. 조회: 호출할 때 마다 객체 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        // 참조값이 다른 것을 확인
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+
+        // 1. 조회: 호출할 때 마다 같은 객체 반환
+        SingletonService singletonService1 = SingletonService.getInstance();
+
+        // 2. 조회: 호출할 때 마다 같은 객체 반환
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        // 참조 값이 같은 것을 확인
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 1. 조회: 호출할 때 마다 같은 객체 반환
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        // 2. 조회: 호출할 때 마다 같은 객체 반환
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조 값이 같은 것을 확인
+        assertThat(memberService1).isSameAs(memberService2);
+    }
+}
