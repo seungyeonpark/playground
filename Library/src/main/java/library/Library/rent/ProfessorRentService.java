d9834df79b435f5e5c3078ce4da8ff250e2eb791
seungyeonpark <siaayeon@gmail.com>
2022-02-21 13:31:11 +0900

    feat: professor rent service

---
+++ Library/src/main/java/library/Library/rent/ProfessorRentService.java
@@ -0,0 +1,19 @@
+package library.Library.rent;
+
+import library.Library.book.Book;
+import library.Library.book.BookRepositoryService;
+import lombok.RequiredArgsConstructor;
+
+@ProfessorComponent
+@RequiredArgsConstructor
+public class ProfessorRentService implements RentService{
+
+    private final BookRepositoryService bookRepositoryService;
+    private static int period = 9;
+
+    @Override
+    public Rent rentBook(Long memberId, String title) {
+        Book book = bookRepositoryService.findBook(title);
+        return new Rent(memberId, book, period);
+    }
+}
