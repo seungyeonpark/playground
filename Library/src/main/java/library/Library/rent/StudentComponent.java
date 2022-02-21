198b6a9346accc90064b20232794c5b867a22ae5
seungyeonpark <siaayeon@gmail.com>
2022-02-21 13:30:49 +0900

    feat: student rent service

---
+++ Library/src/main/java/library/Library/rent/StudentComponent.java
@@ -0,0 +1,9 @@
+package library.Library.rent;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface StudentComponent {
+}
