419d05dbaccd68280f8bc9cbee12d7c8f8c04d5b
seungyeonpark <siaayeon@gmail.com>
2022-02-22 23:05:24 +0900

    fix: typos

--- Library/src/main/java/library/Library/AppConfig.java
+++ Library/src/main/java/library/Library/AppConfig.java
@@ -1,14 +1,12 @@
 package library.Library;
 
 import library.Library.rent.ProfessorComponent;
-import library.Library.rent.StudentComponent;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = StudentComponent.class),
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = ProfessorComponent.class)
 )
 public class AppConfig {
